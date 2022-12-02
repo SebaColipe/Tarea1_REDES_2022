43f429703148ac067282fac196c66012251873a1
SebaColipe <seba_colipe@hotmail.com>
2022-12-02 13:06:05 -0300

    ultimos cambios

--- packet.py
+++ packet.py
@@ -1,5 +1,4 @@
 import struct
-from decimal import Decimal
 import time
 from config import SEVENTY_YEARS_IN_SECONDS, BITE_OFFSET
 
@@ -7,27 +6,29 @@ from config import SEVENTY_YEARS_IN_SECONDS, BITE_OFFSET
 class SNTP:
     HEADER_FORMAT = '> B B B B I I 4s Q Q Q Q'
     LEAP_INDICATOR = 0
-    VERSION_NUMBER = 4
+    VERSION_NUMBER = 3
     MODE = 4
-    STRATUM = 1
+    STRATUM = 2
     FIRST_OCTET = LEAP_INDICATOR << 6 | VERSION_NUMBER << 3 | MODE
     CLIENT_REQUEST = '\x1b' + 47 * '\0'
 
     def __init__(self, time_delta=0):
-        self.received_time = self.get_wrong_time()
-        self.transmit_time = 0
+        self.received_time = self.get_transmit_time()
+        self.originate_time = 0
         self.time_delta = time_delta
 
     def analise_packet(self, received_packet):
-        self.transmit_time = struct.unpack(self.HEADER_FORMAT, received_packet)[10]
+        print("Analise packet: ")
+        self.originate_time = struct.unpack(self.HEADER_FORMAT, received_packet)[10]
+        print(self.originate_time)
 
     def get_server_packet(self, hora_server):
         return struct.pack(self.HEADER_FORMAT, self.FIRST_OCTET,
                            self.STRATUM, 0, 0, 0, 0, b'', 0,
-                           self.transmit_time, self.received_time,
-                           self.get_wrong_time(self.time_delta, hora_server))
+                           self.originate_time, self.received_time,
+                           self.get_transmit_time(self.time_delta, hora_server))
 
-    def get_wrong_time(self, time_delta=0, hora_server=time.time()):
+    def get_transmit_time(self, time_delta=0, hora_server=time.time()):
         now = hora_server
-        wrong_time = now + time_delta
-        return int(Decimal(wrong_time) * BITE_OFFSET)
+        actual_time = now + time_delta
+        return int(actual_time * BITE_OFFSET)
